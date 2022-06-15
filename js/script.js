1af364d0e00f8913f63369c42ded544e5778b337
EwanGrobet <grobet.ewan@gmail.com>
2022-06-15 14:37:16 +0200

    adding somes animation and some real text for the home page and some list for the page musique

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ window.addEventListener('scroll', () => {
         photoEx.classList.add('active')
     }
 })
+
+const photoEx2 = document.querySelector('.photoEx2');
+
+window.addEventListener('scroll', () => {
+    const {scrollTop, clientHeight} =
+    document.documentElement
+
+    const topElementToTopViewport =photoEx2.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        photoEx2.classList.add('active')
+    }
+})
+
+const musique = document.querySelector('.musique');
+
+window.addEventListener('scroll', () => {
+    const {scrollTop, clientHeight} =
+    document.documentElement
+
+    const topElementToTopViewport =musique.getBoundingClientRect().top;
+
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        musique.classList.add('active')
+    }
+})

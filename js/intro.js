ae576046c2b95f0e32c61481975dad9e68ab1a88
EwanGrobet <grobet.ewan@gmail.com>
2022-06-22 14:35:32 +0200

    adding comments for the presentation and img

--- js/intro.js
+++ js/intro.js
@@ -7,10 +7,15 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
 
 anime.timeline({ loop: false })
   .add({
+    /*déclenche les classes*/
     targets: '.ml3 .letter',
+    /*l'opacity commence a 0%fini a 100%*/
     opacity: [0, 1],
+    /*accélération de l'animation (faible)*/
     easing: "easeInOutQuad",
+    /*Temps de l'animation*/
     duration: 2250,
+    /*Temps avant que l'animation se lance*/
     delay: (el, i) => 150 * (i + 1)
   }).add({
     targets: '.ml3',
--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // slide des photos d'exemples
+//La méthode querySelector() permet de sélectionner le premier élément CSS qui possède la classe photoEx.
 const photoEx = document.querySelector('.photoEx');
 
 window.addEventListener('scroll', () => {
+    //défini les paramètres du scroll et de la hauteur du client
     const { scrollTop, clientHeight } =
         document.documentElement
 
     const topElementToTopViewport = photoEx.getBoundingClientRect().top;
-
+    //des que la class photoEx apparait dans le 80% du champs de vision de la page web, l'animation apparaît
     if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
         photoEx.classList.add('active')
     }
